416cb3a924b4fb82e741a4b46806818a5c12fb18
Luciano <lucco6823@gmail.com>
2021-09-08 16:43:24 +0200

    Scorrere le immagini

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
 Vue.config.devtools = true;
+
+const app = new Vue({
+    el: "#root",
+    data: {
+        counterImages: 0,
+        images: [
+            "img/image1.jpg",
+            "img/image2.jpg",
+            "img/image3.jpg",
+            "img/image4.jpg"
+        ]
+    },
+    methods: {
+        prevImage(){
+            this.counterImages -= 1;
+            if(this.counterImages < 0) this.counterImages = (this.images.length -1);
+        },
+        nextImage(){
+            this.counterImages += 1;
+            if(this.counterImages == (this.images.length - 1)) this.counterImages = 0;
+        }
+    }
+})
